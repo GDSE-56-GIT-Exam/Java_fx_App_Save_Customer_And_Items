4c930065b8a1dbbd201259e6b84469fcbfd4a9a0
Your Name <you@example.com>
2021-10-09 10:48:37 +0530

    Create Add Item UI

---
+++ src/lk/ijse/controller/AddItemFormController.java
@@ -0,0 +1,31 @@
+/*----------------------------------------------------------------------------------------------
+ *Copyright(C) GDSE-56-GIT Exam. All rights reserved.
+ *Licensed under the MIT License. See License, txt in the project root for license information.
+ *---------------------------------------------------------------------------------------------*/
+
+package lk.ijse.controller;
+
+import com.jfoenix.controls.JFXButton;
+import com.jfoenix.controls.JFXTextField;
+import javafx.event.ActionEvent;
+import javafx.scene.layout.AnchorPane;
+
+/**
+ * @author Tharushi Welarathna <nirmanitharushi1@gmail.com>
+ * @since 10/9/2021
+ */
+public class AddItemFormController {
+    public AnchorPane root;
+    public JFXButton btnAddNewItem;
+    public JFXButton btnSaveItem;
+    public JFXTextField txtItemCode;
+    public JFXTextField txtDescription;
+    public JFXTextField txtQtyOnHand;
+    public JFXTextField txtPrice;
+
+    public void btnAddNewItemOnAction(ActionEvent actionEvent) {
+    }
+
+    public void btnSaveItemOnAction(ActionEvent actionEvent) {
+    }
+}
