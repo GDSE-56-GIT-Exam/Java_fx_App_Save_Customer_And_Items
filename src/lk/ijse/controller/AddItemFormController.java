5e52f4c00b7e834000545cf7a6d0fb7e3535ffd4
Your Name <you@example.com>
2021-10-09 10:59:59 +0530

    Update DBConnection & Refactor AddItemController.

--- src/lk/ijse/controller/AddItemFormController.java
+++ src/lk/ijse/controller/AddItemFormController.java
@@ -8,7 +8,17 @@ package lk.ijse.controller;
 import com.jfoenix.controls.JFXButton;
 import com.jfoenix.controls.JFXTextField;
 import javafx.event.ActionEvent;
+import javafx.scene.control.Alert;
+import javafx.scene.control.ButtonType;
 import javafx.scene.layout.AnchorPane;
+import javafx.scene.paint.Paint;
+import lk.ijse.db.DBConnection;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import java.util.regex.Pattern;
 
 /**
  * @author Tharushi Welarathna <nirmanitharushi1@gmail.com>
@@ -22,10 +32,48 @@ public class AddItemFormController {
     public JFXTextField txtDescription;
     public JFXTextField txtQtyOnHand;
     public JFXTextField txtPrice;
+    private boolean addNew;
 
     public void btnAddNewItemOnAction(ActionEvent actionEvent) {
+        txtItemCode.requestFocus();
+        addNew = true;
     }
 
     public void btnSaveItemOnAction(ActionEvent actionEvent) {
+        if (addNew){
+
+            try {
+
+                Connection connection = DBConnection.getInstance().getConnection();
+
+                PreparedStatement pstm = connection.prepareStatement("INSERT INTO Product VALUES (?,?,?,?,?,?,?,?)");
+
+
+                                        pstm.setObject(1, txtItemCode.getText());
+                                        pstm.setObject(2, txtDescription.getText());
+                                        pstm.setObject(3, txtQtyOnHand.getText());
+                                        pstm.setObject(4, txtPrice.getText());
+
+
+
+                                        int affectedRows = pstm.executeUpdate();
+
+                                        if (affectedRows > 0){
+
+                                        }else{
+                                            new Alert(Alert.AlertType.ERROR, "Failed to add the item", ButtonType.OK).show();
+                                        }
+
+            } catch (Exception ex) {
+                Logger.getLogger(AddItemFormController.class.getName()).log(Level.SEVERE, null, ex);
+            }
+
+
+
+        }else{
+
+
+
+        }
     }
 }
--- src/lk/ijse/db/DBConnection.java
+++ src/lk/ijse/db/DBConnection.java
@@ -5,9 +5,33 @@
 
 package lk.ijse.db;
 
+import java.sql.Connection;
+import java.sql.DatabaseMetaData;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 /**
  * @author Tharushi Welarathna <nirmanitharushi1@gmail.com>
  * @since 10/9/2021
  */
 public class DBConnection {
+    private static DBConnection dbConnection;
+    private Connection connection;
+
+    private DBConnection() throws ClassNotFoundException, SQLException {
+        Class.forName("com.mysql.jdbc.Driver");
+        connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/supermarket1","root","1234");
+    }
+
+    public static DBConnection getInstance() throws Exception{
+        if (dbConnection == null){
+            dbConnection = new DBConnection();
+        }
+        return dbConnection;
+    }
+
+    public Connection getConnection(){
+        return connection;
+    }
+
 }
