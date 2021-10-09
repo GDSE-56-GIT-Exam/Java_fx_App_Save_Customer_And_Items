5192bf3b0ab84ac7608cc592e8cfb1d5a62ab9e2
Your Name <you@example.com>
2021-10-09 09:46:50 +0530

    Added DBConnaction Class

---
+++ src/lk/ijse/db/DBConnection.java
@@ -0,0 +1,13 @@
+/*----------------------------------------------------------------------------------------------
+ *Copyright(C) GDSE-56-GIT Exam. All rights reserved.
+ *Licensed under the MIT License. See License, txt in the project root for license information.
+ *---------------------------------------------------------------------------------------------*/
+
+package lk.ijse.db;
+
+/**
+ * @author Tharushi Welarathna <nirmanitharushi1@gmail.com>
+ * @since 10/9/2021
+ */
+public class DBConnection {
+}
